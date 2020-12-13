7bcec03ab86898222e158d01142b0a6c6789e653
dzimiks <vana997@gmail.com>
2020-12-13 12:48:10 +0100

    Preview data from JSON

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
 import React from 'react';
 import './App.css';
 
+// import allUsers from './scripts/data/users.json';
+import balkanUsers from './scripts/data/balkanUsers.json';
+
 function App() {
   return (
     <div className="App">
       <div className="container">
         <div className="row">
           <div className="col-12">
-            <h3>TikTok Scraper</h3>
+            <h1 className="text-center">
+              TikTok Scraper
+            </h1>
           </div>
+        </div>
+
+        <div className="row">
+          {balkanUsers.map((post) => (
+            <div className="col-md-4 mb-3">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">
+                    {post.authorMeta.name}
+                  </h5>
+
+                  <h6 className="card-subtitle mb-2 text-muted">
+                    {post.authorMeta.signature}
+                  </h6>
+
+                  <p className="card-text">
+                    {post.text}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
